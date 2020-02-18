813c5af0d62750373093b5c46ca3ab4c051aeb71
Anna Gordon <annetta@email.com>
2020-02-18 22:21:16 +0000

    Added delete button with post

--- express_server.js
+++ express_server.js
@@ -44,10 +44,14 @@ app.post("/urls/new", (req, res) => {
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.shortURL };
   res.render('urls_show', templateVars);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
