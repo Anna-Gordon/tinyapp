12a6ccd8cd8cd5f02ea8c76ebdb315fc6c31c514
Anna Gordon <annetta@email.com>
2020-02-21 07:15:38 +0000

    test file

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -16,22 +16,41 @@ const testUsers = {
   }
 };
 
+// const urlDatabase = {
+//   "b2xVn2": {longURL: "http://www.lighthouselabs.ca", userID: "userRandomID"},
+//   "9sm5xK": {longURL: "http://www.google.com", userID: "user2RandomID"}
+// };
+
 describe('getUserByEmail', function() {
+
   it('should return a user with valid email', function() {
     const user = getUserByEmail("user@example.com", users);
     const expectedOutput = "userRandomID";
     assert.equal(user, expectedOutput);
   });
 
-  it('should return error message if a user didn\'t provide an email', function() {
-    const user = getUserByEmail("user@example.com", users);
-    const expectedOutput = "Bad request. Provide email or password";
-    assert.equal(user, expectedOutput);
+  it('should return error message if a user provided wrong email', function() {
+    const user = getUserByEmail("userW@example.com", users);
+    const expectedOutput = false;
+    assert.equal(user, expectedOutput, "Bad request. Provide email or password");
   });
 
-  it('should return error message if a user didn\'t provide an email', function() {
-    const user = getUserByEmail("user@example.com", users);
-    const expectedOutput = "Bad request. Provide email or password";
-    assert.equal(user, expectedOutput);
+  it('should return array of objects if userID found in message urlDatabase', function() {
+    const user = urlsForUser(urlDatabase, 'userRandomID') ;
+    const expectedOutput = [{shortURL: "b2xVn2", longURL:"http://www.lighthouselabs.ca"}];
+    assert.deepEqual(user, expectedOutput);
   });
+
+  it('should return random shortURL 6 characters long', function() {
+    const user = randomShortURL();
+    const expectedOutput = 'WwWW1Q';
+    assert.notEqual(user, expectedOutput);
+  });
+
+  // it('should return random shortURL 6 characters long', function() {
+  //   const user = userObject(users, 'user@example.com') ;
+  //   const expectedOutput = 'WwWW1Q';
+  //   assert.deepEqual(user, expectedOutput);
+  // });
+
 });
